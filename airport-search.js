a1a3763f74a27df4ddc5218043354a5e20607e45
banana41golf <mathewhallihan@gmail.com>
2024-12-11 14:24:53 -0500

    Update airport-search.js

--- airport-search.js
+++ airport-search.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     } catch (error) {
       console.error("Failed to fetch METAR data:", error);
-      metarSPAN.textContent = "Unable to fetch METAR data.";
+      document.getElementById("rawMETAR").innerText = "Unable to fetch METAR data";
     }
   }
 
