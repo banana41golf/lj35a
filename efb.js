50185c4e0527f9c4eda47daf968527042a90c478
banana41golf <mathewhallihan@gmail.com>
2024-12-11 21:52:50 -0500

    trim

--- efb.js
+++ efb.js
@@ -343,6 +343,7 @@ if(gw > 15300) {
     document.getElementById("vref-output").innerText = vref ? `${Math.round(vref)} knots` : "N/A";
     document.getElementById("ldaa-output").innerText = ldaa ? `${Math.round(ldaa)} feet` : "N/A";
     document.getElementById("fact-output").innerText = fact ? `${Math.round(fact)} feet` : "N/A";
+    document.getElementById("trim-output").innerText = trim ? `${Math.round(trim)} units` : "N/A";
 
     const gustFactor = parseInt(document.getElementById("gust-factor").value);
     const vapp = gustFactor + vref
