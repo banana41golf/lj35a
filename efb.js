1c9b6fd6175182b9092ab23555a0abcc2d41442f
banana41golf <mathewhallihan@gmail.com>
2024-12-11 19:45:58 -0500

    Update efb.js

--- efb.js
+++ efb.js
@@ -326,7 +326,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const fact = trilinearInterpolationDistance(factData, oat, elevation, gw);
 
     //Update HTML forms
-    document.getElementById("n1-output").innerText = n1 ? `${n1.toFixed(2)} %` : "N/A";
+    document.getElementById("n1-output").innerText = n1 ? `${n1.toFixed(1)}%` : "N/A";
     document.getElementById("distance-output").innerText = distance ? `${Math.round(distance)} ft` : "N/A";
     document.getElementById("v1-output").innerText = v1 ? `${Math.round(v1)} knots` : "N/A";
     document.getElementById("vr-output").innerText = vr ? `${Math.round(vr)} knots` : "N/A";
