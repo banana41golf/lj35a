5849464c1a2e2ff1567355597b923f79393d1f21
banana41golf <mathewhallihan@gmail.com>
2024-12-11 21:15:28 -0500

    Update efb.js

--- efb.js
+++ efb.js
@@ -339,12 +339,15 @@ if(gw > 15300) {
     document.getElementById("vref-output").innerText = vref ? `${Math.round(vref)} knots` : "N/A";
     document.getElementById("ldaa-output").innerText = ldaa ? `${Math.round(ldaa)} feet` : "N/A";
     document.getElementById("fact-output").innerText = fact ? `${Math.round(fact)} feet` : "N/A";
+
+    const gustFactor = document.getElementById("gust-factor").value;
+    const vapp = gustFactor + vref
+  
+    document.getElementById("vapp-output").innerText = vapp ? `${Math.round(vapp)} knots` : "N/A";
+  
+
   });
 
-  const gustFactor = document.getElementById("gust-factor").value;
-  const vapp = gustFactor + vref
-
-  document.getElementById("vapp-output").innerText = vapp ? `${Math.round(vapp)} knots` : "N/A";
 
   loadData();
 });
